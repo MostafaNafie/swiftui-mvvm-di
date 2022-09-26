ede2ba2e65909a769cf7dcf51df89c580d90be5b
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2022-09-26 21:16:44 +0200

    Add View+onViewDidLoad extension

---
+++ BreakingBad/Extensions/View+onViewDidLoad.swift
@@ -0,0 +1,29 @@
+//
+//  View+onViewDidLoad.swift
+//  BreakingBad
+//
+//  Created by Mostafa Nafie on 26/09/2022.
+//
+
+import SwiftUI
+
+struct ViewDidLoadModifier: ViewModifier {
+    @State private var viewDidLoad = false
+    let action: (() -> Void)?
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                if viewDidLoad == false {
+                    viewDidLoad = true
+                    action?()
+                }
+            }
+    }
+}
+
+extension View {
+    func onViewDidLoad(perform action: (() -> Void)? = nil) -> some View {
+        self.modifier(ViewDidLoadModifier(action: action))
+    }
+}
