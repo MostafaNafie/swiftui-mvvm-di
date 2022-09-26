009cedaac496a42408d096696d0416b18435bfd1
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2022-09-26 12:00:07 +0200

    Initial commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Mostafa Nafie on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
