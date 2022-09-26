ceac6c37faa4bbd48c10e679216d93ce799492d3
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2022-09-26 21:15:43 +0200

    Add View+isHidden extension

---
+++ BreakingBad/Extensions/View+isHidden.swift
@@ -0,0 +1,18 @@
+//
+//  View+isHidden.swift
+//  BreakingBad
+//
+//  Created by Mostafa Nafie on 26/09/2022.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder func isHidden(_ isHidden: Bool) -> some View {
+        if isHidden {
+            self.hidden()
+        } else {
+            self
+        }
+    }
+}
