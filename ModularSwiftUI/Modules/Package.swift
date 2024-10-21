066e151f79053f0cf6cee21f0ee1f96bcb271fa4
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2024-10-21 11:01:43 +0300

    Refactor SPM manifest file

--- ModularSwiftUI/Modules/Package.swift
+++ ModularSwiftUI/Modules/Package.swift
@@ -1,13 +1,22 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+let main: [Target] = [
+    .target(
+        name: "Main",
+        dependencies: [
+            "CharacterList",
+            "CharacterDetails",
+            "Networking"
+        ]
+    )
+]
 
 let features: [Target] = [
     .target(
         name: "CharacterList",
         dependencies: [
-            "Kingfisher",
             "NetworkingInterface",
             "Utilities",
             "Models",
@@ -17,7 +26,7 @@ let features: [Target] = [
     .target(
         name: "CharacterDetails",
         dependencies: [
-            "Kingfisher",
+            "Utilities",
             "Models",
         ],
         path: "Sources/Features/CharacterDetails"
@@ -36,7 +45,11 @@ let common: [Target] = [
         name: "NetworkingInterface"
     ),
     .target(
-        name: "Utilities"
+        name: "Utilities",
+        dependencies: [
+            "Swinject",
+            "Kingfisher",
+        ]
     ),
 ]
 
@@ -47,36 +60,30 @@ let tests: [Target] = [
     ),
 ]
 
+let thirdPartyDependencies: [Package.Dependency] = [
+    .package(
+        url: "https://github.com/Swinject/Swinject.git",
+        from: "2.0.0"
+    ),
+    .package(
+        url: "https://github.com/onevcat/Kingfisher.git",
+        from: "7.0.0"
+    ),
+]
+
 let package = Package(
     name: "Modules",
     platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Modules",
-            targets: [
-                "CharacterList",
-                "CharacterDetails",
-                "Models",
-                "Networking",
-                "NetworkingInterface",
-                "Utilities",
-            ]
+            targets: ["Main"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/onevcat/Kingfisher.git",
-            from: "7.0.0"
-        ),
-    ],
+    dependencies: thirdPartyDependencies,
     targets:
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        main +
         features +
         common +
         tests
 )
-
